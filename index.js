a3f72775645cf41da27c3b8e72d10794dee1ae87
norristown <jarvasity@gmail.com>
2024-03-14 21:43:25 -0400

    first commit for backend

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const Post = require('./models/post');
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('test');
+});
+
+app.get('/api/posts', (_req, res) => {
+  Post.find({}).then((posts) => res.json(posts));
+});
+
+app.get('/api/posts/:id', (req, res) => {
+  Post.findById(req.params.id).then((post) => res.json(post));
+});
+
+app.post('/api/posts', (req, res) => {
+  const { body } = req;
+
+  const post = new Post({
+    title: body.title,
+    content: body.content,
+  });
+
+  post.save().then((savedPost) => {
+    res.json(savedPost);
+  });
+});
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ models/post.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+console.log('connecting to', process.env.TEST_MONGODB_URI);
+mongoose
+  .connect(process.env.TEST_MONGODB_URI)
+  .then((result) => console.log('connected to MongoDB'))
+  .catch((error) => console.log('error connectin to MongoDB: ', error.message));
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+postSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Post', postSchema);
