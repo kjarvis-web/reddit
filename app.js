011405e06402deb957701b58b8e358d0309f9095
norristown <jarvasity@gmail.com>
2024-03-16 21:17:12 -0400

    backend test for users added using node test

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 const postsRouter = require('./controllers/posts');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
+const usersRouter = require('./controllers/users');
 
 mongoose.set('strictQuery', false);
 
@@ -27,6 +28,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use('/api/posts', postsRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
