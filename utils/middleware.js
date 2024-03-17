8cbf5e2f24c571401db5efcf043216fac3fe8e91
norristown <jarvasity@gmail.com>
2024-03-17 16:05:40 -0400

    added token expiration errorhandler

--- utils/middleware.js
+++ utils/middleware.js
@@ -27,6 +27,9 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'JsonWebTokenError') {
     return response.status(400).json({ error: 'token missing or invalid' });
   }
+  if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({ error: 'token expired' });
+  }
 
   next(error);
 };
