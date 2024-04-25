f04c392993a68e97734991fdb38988f862b00098
norristown <jarvasity@gmail.com>
2024-04-25 19:19:31 -0400

    error message is displayed on signup submit if username is already taken

--- controllers/users.js
+++ controllers/users.js
@@ -2,21 +2,25 @@ const bcrypt = require('bcrypt');
 const usersRouter = require('express').Router();
 const User = require('../models/user');
 
-usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body;
+usersRouter.post('/', async (request, response, next) => {
+  try {
+    const { username, name, password } = request.body;
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(password, saltRounds);
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  });
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    });
 
-  const savedUser = await user.save();
+    const savedUser = await user.save();
 
-  response.status(201).json(savedUser);
+    response.status(201).json(savedUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
 usersRouter.get('/', async (request, response) => {
